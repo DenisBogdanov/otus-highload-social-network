8f2764b548a0943733157fb6937ef50c0acf4e7e
Denis Bogdanov <bogdanov.math@gmail.com>
2022-04-09 18:33:33 +0300

    Initial commit

---
+++ src/main/java/com/bogdanium/social/network/OtusHighloadSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.bogdanium.social.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusHighloadSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtusHighloadSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bogdanium/social/network/OtusHighloadSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogdanium.social.network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusHighloadSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
