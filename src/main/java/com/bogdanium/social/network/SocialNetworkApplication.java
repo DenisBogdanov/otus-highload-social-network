25aaf69ed689cb383378e47ef4853b6f73666e9c
Denis Bogdanov <bogdanov.math@gmail.com>
2022-05-24 21:51:45 +0300

    Домашнее задание 1 | Заготовка для социальной сети
    Удалил TODO.

--- src/main/java/com/bogdanium/social/network/SocialNetworkApplication.java
+++ src/main/java/com/bogdanium/social/network/SocialNetworkApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// TODO Bogdanov -- deploy to Heroku, https://www.youtube.com/watch?v=q7e4A9pfNrk
 public class SocialNetworkApplication {
 
     public static void main(String[] args) {
